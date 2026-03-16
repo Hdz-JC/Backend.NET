c26fbd1e9ca5d4b310c0fc401642869cf941566d
Hdz-JC <a22150084@itmexicali.edu.mx>
2026-03-15 17:27:27 -0700

    Adding a new dbSet for products table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using ECommerce.Models;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicactionDbContext : DbContext
@@ -5,4 +6,5 @@ public class ApplicactionDbContext : DbContext
     public ApplicactionDbContext(DbContextOptions<ApplicactionDbContext> options) : base(options)
     { }
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
 }
