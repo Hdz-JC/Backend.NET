6a5d82c4f807379b85c1b5852338ebe8ed3a40cc
Hdz-JC <a22150084@itmexicali.edu.mx>
2026-03-24 13:19:03 -0700

    Product functions

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ var dbConnectionString = builder.Configuration.GetConnectionString("ConexionSql"
 builder.Services.AddDbContext<ApplicactionDbContext>(options => options.UseSqlServer(dbConnectionString));
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddControllers();
